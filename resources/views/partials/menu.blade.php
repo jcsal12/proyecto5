6f489f1cb72430ddc5baf0ddb4d683c5ee936581
Alessandro <less013015@gmail.com>
2022-12-13 09:58:20 +0100

    CSS actualizado, bugs resueltos y creada vista contacto

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -3,7 +3,7 @@
     <div class="navitem"><a href="/">Inicio</a></div>
     <div class="navitem"><a href="/servicios">Servicios</a></div>
     <div class="navitem"><a href="#">Clientes</a></div>
-    <div class="navitem"><a href="#">Contacto</a></div>
+    <div class="navitem"><a href="/contacto">Contacto</a></div>
     <div class="containerRRSS">
       <div class="RRSS"><a href="#"><img src="/assets/img/instagram.png" alt="instagram"></a></div>
       <div class="RRSS"><a href="#"><img src="/assets/img/facebook.png" alt="facebook"></a></div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="/assets/img/favicon.ico" type="image/x-icon">
-  <link rel="stylesheet" href="/assets/css/style.css">
-  <title>Advaced Live Design</title>
-</head>
-<body>
-    @include('partials.menu')
-  <div id="portada">
-    <div><img id="logoPortada" src="/assets/img/logo.png" alt=""></div>
-  </div>
-
+@extends('layouts.master')
+@section('content')
   <div id="presentacion">
     <h1>Creamos una web económica que genere clientes</h1>
     <img src="/assets/img/portada.png" alt="imgPortada">
@@ -44,41 +31,46 @@
       </div>
     </div>
   </section>
-
-</body>
-<footer>
-  <div class="columna center">
-    <h4><a href="#">Home</a></h4>
-    <h4><a href="#">Servicios</a></h4>
-    <h4><a href="#">Clientes</a></h4>
-    <h4><a href="#">Contacto</a></h4>
-  </div>
-  <div class="columna center">
-    <p>¡Hola! Hacemos una reunion inicial?</p>
-    <ul>
-      <li><b>Escribenos un email aqui</b></li>
-      <li>Atencion al Cliente: (+34)999-999-999</li>
-    </ul>
-  </div>
-  <div class="columna center">
-    <img class="logo" src="assets/img/logo.png" alt="">
-  </div>
-  <div class="columna">
-    <p><b>Aviso Legal y Políticos</b></p>
-    <ul>
-      <li>Aviso Legal</li>
-      <li>Política de Privacidad</li>
-      <li>Política de Cookies</li>
-    </ul>
-  </div>
-  <div class="columna center">
-    <h4>Redes Sociales</h4>
-
-    <a href="#"><img class="social-media" src="/assets/img/facebook.svg" alt=""></a>
-    <a href="#"><img class="social-media" src="/assets/img/instagram.svg" alt=""></a>
-    <a href="#"><img class="social-media" src="/assets/img/twitter.svg" alt=""></a>
-    <a href="#"><img class="social-media" src="/assets/img/linkedin.svg" alt=""></a>
-  </div>
-</footer>
-<p>© 2022 - Todos los derechos reservados</p>
-</html>
+  <section class="container">
+    <div class="text"> Contacta con nosotros </div>
+    <form action="#">
+      <div class="form-row">
+        <div class="input-data">
+          <input type="text" required>
+          <div class="underline"></div>
+          <label>Nombre</label>
+        </div>
+        <div class="input-data">
+          <input type="text" required>
+          <div class="underline"></div>
+          <label>Apellidos</label>
+        </div>
+      </div>
+      <div class="form-row">
+        <div class="input-data">
+          <input type="text" required>
+          <div class="underline"></div>
+          <label>Correo electronico</label>
+        </div>
+        <div class="input-data">
+          <input type="text" required>
+          <div class="underline"></div>
+          <label>Numero de telefono</label>
+        </div>
+      </div>
+      <div class="form-row">
+        <div class="input-data textarea">
+          <textarea cols="30" rows="10" required></textarea>
+          <div class="underline"></div>
+          <label>Consulta</label>
+        </div>
+      </div>
+      <div class="form-row submit-btn">
+        <div class="input-data">
+          <div class="inner"></div>
+          <input type="submit" value="Enviar">
+        </div>
+      </div>
+    </form>
+  </section>
+@stop
