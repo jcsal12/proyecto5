f8d44a4d2445efcf4c1d487cd358b7fd28d54477
Juan <juancarlossandoval122@gmail.com>
2023-02-16 12:57:24 +0100

    Desplegando React en Laravel

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return view('react');
+});
+
+Route::any('dashboard/{any?}', function () {
+    return view('react');
 });
 
 require __DIR__.'/auth.php';
